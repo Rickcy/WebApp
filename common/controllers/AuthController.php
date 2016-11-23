447e95a749ab17f166a9e1003559345850f840f4
Rickcy <kuden.and.ko@gmail.com>
2016-11-23 15:00:33 +0700

    AddFunc

---
+++ common/controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User11
+ * Date: 23.11.2016
+ * Time: 11:39
+ */
+
+namespace common\controllers;
+
+
+class AuthController
+{
+
+}
---
+++ frontend/modules/cabinet/views/advert/step2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User11
+ * Date: 23.11.2016
+ * Time: 12:23
+ */
